404e150bcfec8bdba86f89b7f5d08a7a8b958f47
zyrobit <marcel.griesser106@gmail.com>
2023-10-05 00:51:55 +0200

    Created playRound function
    compares players choice against computers and announces the winner

--- index.js
+++ index.js
@@ -8,4 +8,29 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+function playRound(playerChoice, computerChoice) {
+
+    playerChoice = playerChoice.toLowerCase();
+
+    const choice = ["rock", "paper", "scissors"];
+
+    if (!choice.includes(playerChoice)) {
+        return "Please choose between rock, paper and scissors!"
+    }
+
+    if (playerChoice === computerChoice) {
+        return "It's a tie!"
+    } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        return `You win! ${playerChoice} beats ${computerChoice}`;
+    } else {
+        return `You lose! ${computerChoice} beats ${playerChoice}`;
+    }
+}
+
+const playerChoice = "RoCk";
+const computerChoice = getComputerChoice();
+console.log(playRound(playerChoice, computerChoice));
