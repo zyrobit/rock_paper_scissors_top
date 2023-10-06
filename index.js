adbbde70ba895c36510357844dd379084d92d54c
zyrobit <marcel.griesser106@gmail.com>
2023-10-06 22:27:31 +0200

    Added game()
    Added a function that plays until someone won 5 rounds

--- index.js
+++ index.js
@@ -31,6 +31,31 @@ function playRound(playerChoice, computerChoice) {
     }
 }
 
-const playerChoice = "RoCk";
-const computerChoice = getComputerChoice();
-console.log(playRound(playerChoice, computerChoice));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; 0 < 5; i++) {
+        const playerChoice = prompt("Enter your choice (rock, paper, or scissors):")
+        const computerChoice = getComputerChoice();
+        const result = playRound(playerChoice, computerChoice);
+
+        console.log(`Round ${i + 1}: ${result}`);
+
+        if (result.includes("win")) {
+            playerScore++;
+        } else if (result.includes("lose")) {
+            computerScore++;
+        }
+
+        if (playerScore >= 5) {
+            console.log("You win the game!");
+            break; // Exit the loop if the player wins
+        } else if (computerScore >= 5) {
+            console.log("Computer wins the game!");
+            break; // Exit the loop if the computer wins
+    }
+}
+}
+
+game();
