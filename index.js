d3ca485d0b1c083bb0e982e0ef8db3b728ac840c
zyrobit <marcel.griesser106@gmail.com>
2023-10-29 23:21:03 +0100

    Removed useless game() function
    Removed the function that was used to play the game in the console.

--- index.js
+++ index.js
@@ -31,35 +31,6 @@ function playRound(playerChoice, computerChoice) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    while (true) {
-        const playerChoice = prompt("Enter your choice (rock, paper, or scissors):");
-        const computerChoice = getComputerChoice();
-        const result = playRound(playerChoice, computerChoice);
-
-        console.log(result);
-
-        if (result.includes("win")) {
-            playerScore++;
-        } else if (result.includes("lose")) {
-            computerScore++;
-        }
-
-        if (playerScore >= 5) {
-            console.log("You win the game!");
-            break; // Exit the loop if the player wins
-        } else if (computerScore >= 5) {
-            console.log("Computer wins the game!");
-            break; // Exit the loop if the computer wins
-        }
-    }
-}
-
-
-
 document.querySelector("#rock").addEventListener("click", () => {
     const computerChoice = getComputerChoice();
     const result = playRound("rock", computerChoice);
