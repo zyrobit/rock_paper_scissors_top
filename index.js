c1e01a6e0f9a2ede2d3f73004e513bc702318204
zyrobit <marcel.griesser106@gmail.com>
2023-10-29 22:23:34 +0100

    Added getComputerChoice function

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    const choices = ["rock", "paper", "scissors"];
+
+    const computerChoice = choices[randomNumber];
+
+    return computerChoice;
+}
+
+function playRound(playerChoice, computerChoice) {
+
+    playerChoice = playerChoice.toLowerCase();
+
+    const choice = ["rock", "paper", "scissors"];
+
+    if (!choice.includes(playerChoice)) {
+        return "Please choose between rock, paper and scissors!"
+    }
+
+    if (playerChoice === computerChoice) {
+        return "It's a tie!"
+    } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        return `You win! ${playerChoice} beats ${computerChoice}`;
+    } else {
+        return `You lose! ${computerChoice} beats ${playerChoice}`;
+    }
+}
+
 function game() {
     let playerScore = 0;
     let computerScore = 0;
